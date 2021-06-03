d828b8ab77f4c803a7616ecb63476439e0d530b7
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-04 04:11:39 +0600

    feat(post by an author): authors posts view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,8 +8,11 @@
                 </a>
             </h1>
 
-            <p>Category:
-                <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
+                in <a
+                    href="/categories/{{
+            $post->category->slug }}"> {{ $post->category->name }}</a>
             </p>
 
             <div>
