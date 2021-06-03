7476ea1d4cf2585400b70e72b2efaace4bdb442e
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-03 16:18:35 +0600

    feat(posts): array-map with dynamic data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $files = File::files(resource_path("posts"));
+    $posts = collect($files)
+        ->map(function ($file) {
+            $document = YamlFrontMatter::parseFile($file);
+
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug,
+            );
+        });
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
+
+Route::get('posts/{post}', function ($slug) {
+    $post = Post::find($slug);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
