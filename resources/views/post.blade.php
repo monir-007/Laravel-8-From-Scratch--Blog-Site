f5b5e13d7fffff3839c9b2d45c78b62dda8515c6
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-03 18:51:08 +0600

    feat(blade-layout-template-inheritance): basics of blade, fix variable php line

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,24 +1,16 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-<article>
-    <h1>{{ $post->title }}</h1>
+@extends('layout')
 
-    <div>
-        {!! $post->body !!}
-    </div>
+    @section('content')
+        <article>
+            <h1>{{ $post->title }}</h1>
 
-</article>
+            <div>
+                {!! $post->body !!}
+            </div>
 
-<a href="/">Go Back</a>
+        </article>
 
-</body>
-</html>
+        <a href="/">Go Back</a>
+    @endsection
+
+
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,25 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-     @foreach ($posts as  $post)
-{{--         @dd($loop) --}}
-    <article class="{{ $loop->even ? 'foobar' : '' }}">
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
-     @endforeach
+@extends('layout')
 
-</body>
-</html>
+    @section('banner')
+        <h1>My Blog</h1>
+    @endsection
+
+    @section('content')
+        @foreach ($posts as  $post)
+            {{--         @dd($loop) --}}
+            <article class="{{ $loop->even ? 'foobar' : '' }}">
+                <h1>
+                    <a href="/posts/{{ $post->slug }}">
+                        {{ $post->title }}
+                    </a>
+                </h1>
+                <div>
+                    {{ $post->excerpt }}
+                </div>
+            </article>
+        @endforeach
+
+    @endsection
+
+
+
