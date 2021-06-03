7438569108861bd67fe7a79f46bb2ab872237d9c
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-03 16:25:38 +0600

    refactor(posts): array-map with dynamic data

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,15 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts"));
-    $posts = collect($files)
-        ->map(function ($file) {
-            $document = YamlFrontMatter::parseFile($file);
-
-            return new Post(
+    $posts = collect(File::files(resource_path("posts")))
+        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn ($document)=> new Post(
                 $document->title,
                 $document->excerpt,
                 $document->date,
                 $document->body(),
                 $document->slug,
-            );
-        });
+        ));
 
     return view('posts', [
         'posts' => $posts
