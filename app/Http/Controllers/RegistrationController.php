7d3ed65bce64ebc4a08946cfa9b13bd9582df981
Monir Hossain <monir.hossain@northsouth.edu>
2021-06-06 22:13:01 +0600

    feat(user registration): by controller method

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    public function index()
+    {
+        return view('registration.index');
+    }
+
+    public function save()
+    {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'username' => 'required|max:12|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+        User::create($attributes);
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,3 +9,5 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+Route::get('registration', [RegistrationController::class, 'index']);
+Route::post('registration',[RegistrationController::class, 'save']);
